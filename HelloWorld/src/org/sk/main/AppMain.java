a2f8af2aa051dc426944fd437e397475b2a3376f
Sai Karthik <sai032@gmail.com>
2017-02-03 14:21:19 +0530

    commit from actual workspace

--- HelloWorld/src/org/sk/main/AppMain.java
+++ HelloWorld/src/org/sk/main/AppMain.java
@@ -5,5 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("welcome...");
 		System.out.println("to java");
+		int j = 14;
+		System.out.println("j = " + j);
 	}
 }
