ca356a556ae50c70119ed40aaa39883409cdaeec
Sai Karthik <sai032@gmail.com>
2017-02-03 14:02:48 +0530

    commit from pull-git workspace

--- HelloWorld/src/org/sk/main/AppMain.java
+++ HelloWorld/src/org/sk/main/AppMain.java
@@ -5,5 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("welcome...");
 		System.out.println("to java");
+		int i = 10;
+		System.out.println("i: " + i);
 	}
 }
