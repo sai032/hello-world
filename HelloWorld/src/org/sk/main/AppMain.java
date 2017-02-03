d32cf3587552c5c2400672bb2723733ecc7e677a
Sai Karthik <sai032@gmail.com>
2017-02-03 14:05:54 +0530

    added comments

--- HelloWorld/src/org/sk/main/AppMain.java
+++ HelloWorld/src/org/sk/main/AppMain.java
@@ -5,6 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("welcome...");
 		System.out.println("to java");
+		//pull from remote & push to 'pull-from-git workspace' 
 		int i = 10;
 		System.out.println("i: " + i);
 	}
